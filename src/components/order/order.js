0fe290f0daf17f54ce4a20c314d344577470b130
unknown <sara.mamdouh@symbyo.com>
2024-03-31 19:52:43 +0200

    make it responsive

--- src/components/order/order.js
+++ src/components/order/order.js
@@ -6,7 +6,7 @@ import { listOrderApi } from "../../services/api/order";
 import "./style.css";
 
 const Order = ({ showOrder }) => {
-  const { data: listOrdersApi } = useQuery(listOrderApi);
+  const { data: ordersQuery } = useQuery(listOrderApi);
   const [selectedOrderId, setSelectedOrderId] = useState(null);
 
   const toggleProductList = (orderId) => {
@@ -36,18 +36,29 @@ const Order = ({ showOrder }) => {
             <i className="fas fa-times" onClick={showOrder}></i>
           </div>
           <div className="container">
-            {listOrdersApi != null &&
-              listOrdersApi.orders.map((order) => (
+            {ordersQuery != null &&
+              ordersQuery.orders.map((order) => (
                 <div key={order._id} className="card mb-4">
-                  <div className="card-header" onClick={() => toggleProductList(order._id)}>
+                  <div
+                    className="card-header"
+                    onClick={() => toggleProductList(order._id)}
+                  >
                     <h5>Order ID: {order._id}</h5>
-                    <p>Status: <span className={`btn ${getStatusColor(order.status)}`}>{order.status}</span></p>
+                    <p>
+                      Status:{" "}
+                      <span className={`btn ${getStatusColor(order.status)}`}>
+                        {order.status}
+                      </span>
+                    </p>
                   </div>
                   {selectedOrderId === order._id && (
                     <div className="card-body">
                       <p>Total Price: ${order.totalPrice}</p>
                       <p>Customer: {order.customer.username}</p>
-                      <p>Created At: {new Date(order.created_at).toLocaleString()}</p>
+                      <p>
+                        Created At:{" "}
+                        {new Date(order.created_at).toLocaleString()}
+                      </p>
                       <h5>Products:</h5>
                       <div className="product-list">
                         {order.items.map((item) => (
